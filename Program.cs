ccf7d5a186845608ee6b0eeb6782fa9db8b65b61
Ben Linus Mueller <B.Mueller.inf22@stud.bbbaden.ch>
2024-05-02 12:58:36 +0200

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LB426_LB_Ben
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
